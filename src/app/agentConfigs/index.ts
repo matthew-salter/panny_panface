fb5caa7bae14627b266e46113008a71e1a252202
Adam Bolas <148712055+chickenpawfisherman@users.noreply.github.com>
2025-06-19 13:24:19 +0100

    Delete src directory

--- src/app/agentConfigs/index.ts
+++
@@ -1,8 +0,0 @@
-import { AllAgentConfigsType } from "@/app/types";
-import simpleExample from "./simpleExample";
-
-export const allAgentSets: AllAgentConfigsType = {
-  simpleExample,
-};
-
-export const defaultAgentSetKey = "simpleExample";
