e9b96d99646d816f77b7cc85da90437b120dae18
Adam Bolas <148712055+chickenpawfisherman@users.noreply.github.com>
2025-06-19 14:22:07 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // This will ignore ESLint errors during the build process
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
